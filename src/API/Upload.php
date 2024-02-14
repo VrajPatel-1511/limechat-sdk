952712af588f4a6da0a0ad4d0a7a177ef8c198d4
VrajPatel-1511 <159014228+VrajPatel-1511@users.noreply.github.com>
2024-02-14 13:14:14 +0530

    Update Upload.php

--- src/API/Upload.php
+++ src/API/Upload.php
@@ -6,7 +6,7 @@ use Flits\Limechat\LimechatProvider;
 class Upload extends LimechatProvider {
 
     public $METHOD = "POST";
-    public $URL = 'upload';
+    public $URL = 'create';
 
     function __construct($config) {
         parent::__construct($config);
